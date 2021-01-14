44cbcf5c251fd41aa488e1a62e6c42df9c45873d
QuatreMains <plutokor@gmail.com>
2021-01-14 21:35:36 +0900

    included test code

--- moon_rabbit.py
+++ moon_rabbit.py
@@ -13,7 +13,8 @@ from boto.s3.connection import S3Connection
 app = commands.Bot(command_prefix="#")
 
 discord_api_key = os.environ.get('discord_api_key')
-discord_api_key = os.environ.get('neople_api_key')
+discord_api_key = os.environ.get('neople_api_key')\
+print(discord_api_key)
 browser_options = webdriver.ChromeOptions()
 browser_options.add_argument('headless')
 browser_options.add_argument('window-size=1920x1080')
