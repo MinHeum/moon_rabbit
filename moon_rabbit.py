439aac5996a6452271675cacae0669ccbaa787a1
QuatreMains <plutokor@gmail.com>
2021-01-19 10:57:05 +0900

    set Config Vars

--- moon_rabbit.py
+++ moon_rabbit.py
@@ -18,7 +18,6 @@ discord_api_key = os.environ.get('neople_api_key')
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-print(discord_api_key)
 browser_options = webdriver.ChromeOptions()
 browser_options.add_argument('headless')
 browser_options.add_argument('window-size=1920x1080')
@@ -26,7 +25,8 @@ browser_options.add_argument("disable-gpu")
 browser_options.add_argument('--no-sandbox')
 browser_options.binary_location = GOOGLE_CHROME_PATH
 
-browser = webdriver.Chrome(CHROMEDRIVER_PATH, options=browser_options)
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=browser_options)
 
 
 @app.event
